9eaa78b2efdccf6b58c3e46e275c8147738396e4
MihaiCatana <mihailondon1995@gmail.com>
2023-02-24 16:46:17 +0000

    Extra comments added to get_sign_text.py

--- get_sign_text.py
+++ get_sign_text.py
@@ -2,9 +2,17 @@ import json
 from bs4 import BeautifulSoup
 import requests
 
-with open('signs.json', 'r') as file:
-    data = json.load(file)
 
+# Find which number corrisponds to the right sign
+def find_num(val):
+    # Load the data from the signs.json file
+    with open('signs.json', 'r') as file:
+        data = json.load(file)
+    for key, value in data.items():
+        if value == val:
+            return key
+
+# This function will be used after the find_num has been used as it needs the number that corrisponds to the sign passed by the user. It will then return the text value of the element 'p'
 def get_text(sign_num):
     txt = requests.get(f'https://www.horoscope.com/us/horoscopes/general/horoscope-general-daily-today.aspx?sign={sign_num}')
     # Parse the HTML content
@@ -14,10 +22,3 @@ def get_text(sign_num):
     # Extract the text
     txt_text = txt_h1.text
     return txt_text
-
-def find_num(val):
-    with open('signs.json', 'r') as file:
-        data = json.load(file)
-    for key, value in data.items():
-        if value == val:
-            return key
